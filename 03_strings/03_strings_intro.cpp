ff7a90b9af6da2670449e9c4af30879f523160f1
Luis Garcia <lgarcia1764@floridapoly.edu>
2026-01-16 15:48:36 +0000

    GIT STATUS GIT COMMIT GIT PUSH

---
+++ 03_strings/03_strings_intro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+    // Create a string
+    std::string str = "Hello";
+
+
+    std::cout << str << std::endl;
+    std::cout << " The string size is " << str.size() << std::end;
+
+    // Append another string
+    str += "World";
+
+    std::cout << str << std::endl;
+    std::cout << " The string size is " << str.size() << std::end;
+
+
+
+    return 0;
+}
